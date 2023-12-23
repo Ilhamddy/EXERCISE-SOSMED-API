7478b362bc4faad767dcdf5bee7d4ef2e6535e08
ilhamddy <ilhammaulana9906@gmail.com>
2023-12-23 16:42:07 +0700

    fix: login

--- src/repositories/users/findUserByUsername.ts
+++ src/repositories/users/findUserByUsername.ts
@@ -2,6 +2,9 @@ import prisma from "../../helper/prisma";
 
 export const findUsersByUsername = async (username: string) => {
   try {
+    // const users = await prisma.user.findUnique({
+    //   where: { username },
+    // });
     const users = await prisma.user.findUnique({
       where: { username },
     });
