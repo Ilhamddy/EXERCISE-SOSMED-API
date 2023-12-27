f10188350f5a2fefa6d35c7ba4b74532246695c1
Ilhamddy <139438329+Ilhamddy@users.noreply.github.com>
2023-12-28 00:25:18 +0700

    feat: add feature delete tweet (#12)

--- src/routers/tweetsRouter.ts
+++ src/routers/tweetsRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { createTweetController } from "../controllers/tweets/createTweetController";
 import { getTweetsController } from "../controllers/tweets/getTweetsController";
+import { deleteTweetController } from "../controllers/tweets/deleteTweetController";
 
 const router = express.Router();
 
 router.post("/", createTweetController);
 router.get("/", getTweetsController);
+router.delete("/:id", deleteTweetController);
 
 export default router;
