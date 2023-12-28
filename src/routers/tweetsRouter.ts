1c51de260fafece6905a4be4aa6a1744ad1a6c05
ilhamddy <ilhammaulana9906@gmail.com>
2023-12-29 04:14:45 +0700

    feat: add edit tweet

--- src/routers/tweetsRouter.ts
+++ src/routers/tweetsRouter.ts
@@ -4,13 +4,11 @@ import { getTweetsController } from "../controllers/tweets/getTweetsController";
 import { deleteTweetController } from "../controllers/tweets/deleteTweetController";
 import { updateTweetController } from "../controllers/tweets/updateTweetController";
 
-
 const router = express.Router();
 //router
 router.post("/", createTweetController);
 router.get("/", getTweetsController);
 router.delete("/:id", deleteTweetController);
 router.patch("/:id", updateTweetController);
-
 
 export default router;
