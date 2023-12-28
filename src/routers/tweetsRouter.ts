e7e650d6f3a2950a4116fc0b33dd2fd9dc0e00cb
Ilhamddy <139438329+Ilhamddy@users.noreply.github.com>
2023-12-29 00:51:57 +0700

    Create tweet (#16)
    * feat: add feature create tweet
    
    * feat: add feature create tweet

--- src/routers/tweetsRouter.ts
+++ src/routers/tweetsRouter.ts
@@ -1,14 +1,9 @@
 import express from "express";
 import { createTweetController } from "../controllers/tweets/createTweetController";
-import { getTweetsController } from "../controllers/tweets/getTweetsController";
-import { deleteTweetController } from "../controllers/tweets/deleteTweetController";
-import { updateTweetController } from "../controllers/tweets/updateTweetController";
+
 
 const router = express.Router();
 
 router.post("/", createTweetController);
-router.get("/", getTweetsController);
-router.delete("/:id", deleteTweetController);
-router.patch("/:id", updateTweetController);
 
 export default router;
