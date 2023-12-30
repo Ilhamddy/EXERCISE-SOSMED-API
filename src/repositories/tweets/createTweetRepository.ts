1b137ee4645772e583b3ec5ff862a76f51c243de
Ilhamddy <139438329+Ilhamddy@users.noreply.github.com>
2023-12-30 17:54:51 +0700

    Create tweet (#26)
    * feat: add feature createdat
    
    * create tweet (#25)
    
    * feat: add feature get all tweet
    
    * feat: change get tweet
    
    * Create tweet (#19)
    
    * feat: add feature create tweet
    
    * feat: add feature create tweet
    
    * Get tweet (#20)
    
    * feat: add feature get all tweet
    
    * feat: change get tweet
    
    * Update tweet (#21)
    
    * feat: add feature update tweet
    
    * feat: add name feature
    
    * Get tweet (#24)
    
    * feat: add feat router
    
    * feat: add feat get
    
    * feat: add feature get tweet
    
    * feat: add desc
    
    * feat: add descending
    
    * gettwet (#23)
    
    * Get tweet (#20)
    
    * feat: add feature get all tweet
    
    * feat: change get tweet
    
    * Update tweet (#21)
    
    * feat: add feature update tweet
    
    * feat: add name feature

--- src/repositories/tweets/createTweetRepository.ts
+++ src/repositories/tweets/createTweetRepository.ts
@@ -2,7 +2,7 @@ import prisma from "../../helper/prisma";
 import { ITweet } from "../../types/tweet.type";
 
 export const createTweetRepository = async (body: ITweet) => {
-  const { userId, tweet } = body;
+  const { userId, tweet, createdAt } = body;
   try {
     const tweets = await prisma.tweets.create({
       data: {
